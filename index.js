c82648c9b2c9328358ef17680e8f747dc887bc10
Shivang Agrawal <shivangagrawal4@gmail.com>
2022-08-20 03:32:45 +0530

    Add simple cube mesh with animation

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 console.log('Hello World!', { THREE });
+
+/** Utils Setup */
 const canvas = document.querySelector('.webgl');
 const size = { width: window.innerWidth, height: window.innerHeight };
 
@@ -13,18 +15,37 @@ window.addEventListener('resize', () => {
 	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
+/** Geometry */
+const geometry = new THREE.BoxGeometry();
+
+/** Material */
+const material = new THREE.MeshStandardMaterial({ color: 0x3399BB });
+
+/** Meshes */
+const mesh = new THREE.Mesh(geometry, material);
+
+/** Lights */
+const light = new THREE.PointLight();
+light.position.z = 5;
+
 /** Scene */
 const scene = new THREE.Scene();
+scene.add(mesh);
+scene.add(light);
 
 /** Perspective Camera */
 const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 1000);
+camera.position.z = 5;
 
 /** WebGL Renderer */
-const renderer = new THREE.WebGLRenderer({ canvas: canvas });
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true, antialias: true });
 renderer.setSize(size.width, size.height);
 
 /** render loop */
 const render = () => {
+	mesh.rotation.x += 0.01;
+	mesh.rotation.y += 0.01;
+
 	renderer.render(scene, camera);
 
 	requestAnimationFrame(render);
