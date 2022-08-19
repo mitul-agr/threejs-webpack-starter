d7981753a1eefb47fa0714d39af0e386c3d8c6c5
Shivang Agrawal <shivangagrawal4@gmail.com>
2022-08-20 03:10:39 +0530

    Add window resize event Listener to update camera and render

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 console.log('Hello World!', { THREE });
 const canvas = document.querySelector('.webgl');
+const size = { width: window.innerWidth, height: window.innerHeight };
+
+window.addEventListener('resize', () => {
+	size.width = window.innerWidth;
+	size.height = window.innerHeight;
+
+	camera.aspect = size.width / size.height;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(size.width, size.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 /** Scene */
 const scene = new THREE.Scene();
 
 /** Perspective Camera */
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 0.1, 1000);
 
 /** WebGL Renderer */
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(size.width, size.height);
 
 /** render loop */
 const render = () => {
